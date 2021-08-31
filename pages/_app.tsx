5e12c0dbc71ad23e8ce0f775721f9dabb6d517b6
Hani Husamuddin <hani.husam@gmail.com>
2021-08-31 21:21:46 +0700

    refactor: app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
-export default MyApp
